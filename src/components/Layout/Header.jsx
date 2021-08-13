93a46a30aacd78803daba6d36f1d19d6ccc96b6b
Lucas Peixoto <lspeixoto@stefanini.com>
2021-08-13 08:18:59 -0300

    Inclusão do botão do carrinho

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
-
 import mealsImage from "../../assets/images/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
 const Header = props => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Food Order App</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Algumas opções" />
