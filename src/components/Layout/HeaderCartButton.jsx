7628a8bb3b3f9e52e348dea9499016899870d0d4
Lucas Peixoto <lspeixoto@stefanini.com>
2021-08-15 22:56:30 -0300

    Finalização com o efeito de bump ao adicionar itens

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+	const [btnIsHighlighted, setbtnIsHighlighted] = useState(false);
 	const cartContext = useContext(CartContext);
-
-	const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+	const { items } = cartContext;
+	const numberOfCartItems = items.reduce((currentNumber, item) => {
 		return currentNumber + item.amount;
 	}, 0);
 
+	const btnClasses = `${classes.button} ${
+		btnIsHighlighted ? classes.bump : ''
+	}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setbtnIsHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setbtnIsHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
