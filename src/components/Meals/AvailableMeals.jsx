33d9a750916659ed9f061269c70863503f34d03f
Lucas Peixoto <lspeixoto@stefanini.com>
2021-11-10 21:38:27 -0300

    Inclusão de requisição para obtenção das refeições no firebase

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99,
-	},
-];
-
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map(meal => (
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	//* Funções dentro do useEffect devem ser sincronas, não podendo retornar promises, observables ou serem async
+	//* Se quisermos usar, precisamos criar uma função dentro da nossa função do useEffect
+	useEffect(() => {
+		const fecthMeals = async () => {
+			const response = await fetch(
+				'https://react-http-cacd1-default-rtdb.firebaseio.com/meals.json',
+			);
+
+			const responseData = await response.json();
+
+			const loadedMeals = [];
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+
+			setMeals(loadedMeals);
+
+			setIsLoading(false);
+		};
+
+		fecthMeals();
+	}, []);
+
+	if (isLoading) {
+		return <section className={classes.spinner}></section>;
+	}
+
+	const mealsList = meals.map(meal => (
 		<MealItem
 			id={meal.id}
 			key={meal.id}
